da68dc3cc3d1948e4da67c0664dcf1db2efc203b
SriramVarma4 <78374830+SriramVarma4@users.noreply.github.com>
2023-05-28 16:40:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+// Required modules
+const express = require('express');
+const { Pool } = require('pg');
+
+// Create PostgreSQL connection pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'users',
+  password: 'pass',
+  port: 5432, // Default PostgreSQL port
+});
+
+// Create Express application
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  // Insert the new user into the database
+  pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password], (error) => {
+    if (error) {
+      console.error('Error registering user:', error);
+      res.send('Error registering user');
+    } else {
+      res.redirect('/dashboard');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the user exists in the database
+  pool.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password], (error, result) => {
+    if (error) {
+      console.error('Error logging in:', error);
+      res.send('Error logging in');
+    } else if (result.rows.length === 0) {
+      res.send('Invalid credentials');
+    } else {
+      res.redirect('/dashboard');
+    }
+  });
+});
+
+app.get('/dashboard', (req, res) => {
+  // Render the dashboard view
+  res.render('dashboard');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
